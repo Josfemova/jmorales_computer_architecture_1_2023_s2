31f59133f16966dff34912a3abe894c9d4649e9c
josfemova <josfemova@gmail.com>
2023-10-16 23:24:14 -0600

    proyecto_2: agregar soporte a uso del preprocesador

--- proyecto_2/software/assembler/compiler/compile.sh
+++ proyecto_2/software/assembler/compiler/compile.sh
@@ -2,13 +2,14 @@
 
 rm -rf build
 mkdir build;
-pre0=build/preproc0
-pre1=build/preproc1
-pre2=build/preproc2
-# eliminar lineas vacías 
-sed '/^$/d' $1 > $pre0
-# eliminar espacios
-sed 's/ //g' $pre0 > $pre1
-# eliminar todo después de punto y coma
-sed 's/\;.*/;\n/' $pre1 > $pre2 
-./main < $pre2 
+pre=build/preproc.s
+# eliminar todo después de numeral, lineas vacías
+# y todo depués del punto y coma 
+cpp $1 \
+    | sed 's/\#.*//' \
+    | sed 's/:/;/g' \
+    | sed '/^$/d' \
+    | sed 's/ //g' \
+    | sed 's/\;.*/;/' \
+    > $pre 
+./main < $pre 
